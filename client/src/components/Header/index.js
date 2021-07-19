61610b1405a2b1d40e29b023e720e0213671250f
IgorDavydenko <4_me@li.ru>
2021-07-19 22:42:54 +0300

    update header layout

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,28 +3,28 @@ import { NavLink } from 'react-router-dom'
 
 import './Header.scss' 
 
-import Navigation from './Navigation/index.js'
+import Navigation from '../Navigation/index.js'
 
 const Header = () => {
 
   return (
-    <div className="header-wrapper">
-      <header className="header">
+    <header className="header-wrapper">
+      <div className="header">
         <div className="header__logo">
           <NavLink to="/">
             <img src="/img/logo.jpg" alt=""/></NavLink>
         </div>
         <div className="fund">
-          <div className="fund-status">
+          <div className="fund__item fund__status">
             Автономная некоммерческая организация помощи семьи и детям
           </div>
-          <div className="fund-name">
+          <div className="fund__item fund__name">
             От сердца к сердцу
           </div>
         </div>
         <div className="contacts">
           <div className="contacts__phone">
-          {/* <a href="tel:+7000000000">1-562-867-5309</a> */}
+           {/* <a href="tel:+7000000000">1-562-867-5309</a> */}
             +7 <span className="item_font-red">(930)</span> 401-67-57
           </div>
           <div className="contacts__email">
@@ -34,9 +34,9 @@ const Header = () => {
             г. Воронеж, Ул. Хользунова 107 E
           </div>
         </div>
-        <Navigation/>
-      </header>
-    </div>
+      </div>
+      <Navigation/>
+    </header>
   )
 }
 
