88466890b9e1b6360ba44512b156448a98001b19
IgorDavydenko <4_me@li.ru>
2021-07-23 21:17:45 +0300

    update background

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,22 +10,17 @@ import Header from './Header/index.js'
 import Content from './Content/index.js'
 import Footer from './Footer/index.js'
 
-import backgroundImg from "../assets/background.jpg";
-
-
 function App() {
   return (
     <div>
       <Router>
         <div className="charityApp">
-            <Header />
-            <Content />
-            <Footer />
+          <img className="content__background" src="/img/background.jpg"/>
+          <Header />
+          <Content />
+          <Footer />
         </div>
-        {/* <section className="banner"></section> */}
       </Router>
-      {/* <div className="banner"
-              style={{ backgroundImage: `url(${backgroundImg})` }}/> */}
     </div>
   );
 }
