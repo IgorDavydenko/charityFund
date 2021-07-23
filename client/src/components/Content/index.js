ef9d8bc42956616fbfe85f26c76929dd6a766e49
IgorDavydenko <4_me@li.ru>
2021-07-23 21:19:35 +0300

    clean code

--- client/src/components/Content/index.js
+++ client/src/components/Content/index.js
@@ -10,25 +10,17 @@ import Team from '../Team/index.js'
 
 import './Content.scss'
 import HomePage from '../HomePage/index.js';
-// import backgroundImg from "../../assets/background.jpg";
-
 
 const Content = () => {
   
   return (
     <div className="content-wrapper" >
-      {/* <div className="content__background"
-            style={{ backgroundImage: `url(${backgroundImg})` }}/> */}
-      
-      <img className="content__background" src="/img/background.jpg"/>
-      <div className=".content__background"></div>
       <Switch>
         <Route exact path='/' component={ HomePage }/>
         <Route exact path='/team' component={ Team }/>
         <Route exact path='/documents' component={ Documents }/>
         <Route exact path='/requisites' component={ Requisites }/>
-      </Switch>
-
+      </Switch> 
     </div>
   )
 }
