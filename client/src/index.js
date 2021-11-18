8b7a4e2bf1bc55f57f047b07ff82408cc468a80a
IgorDavydenko <4_me@li.ru>
2021-11-18 17:16:26 +0300

    some refactor

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Header from './components/Header/index.js'
+import Content from './components/Content/index.js'
+import Hands from './components/StyleBlock/Hands'
+
+import './style.scss'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <div className="charityApp-wrapper">
+            <Router>
+                <Header />
+                <Content />
+            </Router>
+            <Hands />
+        </div>
     </React.StrictMode>
   , document.getElementById('charityFundApp'));
