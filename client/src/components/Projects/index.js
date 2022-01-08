ccfc35e265189562dfd361631da5d617aab6f2a7
IgorDavydenko <4_me@li.ru>
2022-01-08 22:21:09 +0300

    done projects

--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -9,31 +9,29 @@ const Projects = () => {
 
         <div className="project">
           <div className="orhpanage">
-            <h3 className="project__title">
-              Детские учреждения
-            </h3>
             <div className="project__content">
-              <div className="project__description">
+              <h3 className="project__title">
+                Детские учреждения
+              </h3>
+              <p className="project__description">
                 Главная цель создания Автономной некоммерческой организации помощи семьи и детям «От сердца к сердцу» - объединить добрых и отзывчивых людей, способных помочь. И вместе оказать помощь детским учреждениям, которые нуждаются в нашей поддержке.<br/>
                 В России много волонтёрских движений, направленных на помощь нуждающихся и обездоленных. К сожалению, в нашем обществе есть проблемы (и их не мало), которые решаются долгое время, и справиться с ними каждому отдельному человеку своими силами не возможно.<br/>
                 Практически все детские дома нуждаются в конкретной адресной помощи. И чем дальше от Воронежа находится учреждение, тем больше оно нуждается в нашей помощи. Это и вещи, и спортивный инвентарь, и бытовые приборы, и медицинское оборудование и многое другое необходимое в нашей повседневной жизни.<br/>
-              </div>
-              <div className="project__slogan">
+              </p>
+              <p className="project__slogan">
                 Только вместе мы сможем больше!
-              </div>
+              </p>
             </div>
             <div className="orhpanage__imgs">
-              <div class="orhpanage__img img__primary">
+              <div class="orhpanage__img orhpanage__img_primary">
                 <img src="/img/projects/orphanage/Kiev_172667-mini.jpg"></img>
               </div>
-              <div class="img__secondary">
-                <div  class="secondary__item">
+              <div class="orhpanage__img orhpanage__img_secondary">
                   <img src="/img/projects/orphanage/i.jpg"></img>
                 </div>
-                <div class="secondary__item">
+                <div class="orhpanage__img orhpanage__img_secondary">
                   <img src="/img/projects/orphanage/scale_1200.jpg"></img>
                 </div>
-              </div>
             </div>
           </div>
         </div>
