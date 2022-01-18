19714178bf688271056a99327b7d661827abe58e
IgorDavydenko <4_me@li.ru>
2022-01-18 17:52:09 +0300

    update documents

--- client/src/components/Documents/index.js
+++ client/src/components/Documents/index.js
@@ -1,29 +1,29 @@
 import React from 'react'
-
 import './Documents.scss'
-import Card from './Card'
 
 const Documents = (props) => {
-
   return (
     <div className="documents">
-      <Card 
-          preview="/img/docs/Licence.jpeg"
-          path="/files/Licence.pdf"
-          title="Свидетельство о регистрации"/>
-      <Card 
-          preview="/img/docs/FNS.jpeg"
-          path="/files/FNS.pdf"
-          title="Свидетельство о постановке на налоговый учет"/>
-      <Card 
-          preview="/img/docs/EGR.jpeg"
-          path="/files/EGR.pdf"
-          title="Выписка ЕГР"/>
-      <Card 
-          preview="/img/docs/Ustav.jpeg"
-          path="/files/Ustav.pdf"
-          title="Устав"/>
-      {/* <a href="/files/Licence.pdf" target="_blank">licence</a> */}
+      <div className="document">
+        <a href="/files/Licence.pdf" target="_blank">
+        Свидетельство о регистрации
+        </a>
+      </div>
+      <div className="document">
+        <a href="/files/FNS.pdf" target="_blank">
+        Свидетельство о постановке на налоговый учет
+        </a>
+      </div>
+      <div className="document">
+        <a href="/files/EGR.pdf" target="_blank">
+        Выписка ЕГР
+        </a>
+      </div>
+      <div className="document">
+        <a href="/files/Ustav.pdf" target="_blank">
+        Устав
+        </a>
+      </div>
     </div>
   )
 }
