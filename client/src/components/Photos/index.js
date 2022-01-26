df55810383aa48f3f9618fa370692a3435c08524
IgorDavydenko <4_me@li.ru>
2022-01-26 15:23:02 +0300

    add new events layout

--- client/src/components/Photos/index.js
+++ client/src/components/Photos/index.js
@@ -1,64 +1,25 @@
 import React from 'react'
 import { SRLWrapper } from "simple-react-lightbox";
 
-// import Card from './Card/index.js'
-
 import './Photos.scss'
 
 const Photos = () => {
 
-  const WINDOW_WIDTH = window.innerWidth;
-
-  // const fullScreenView = (e) => {
-  //   // console.log(e.target);
-  //   console.log(WINDOW_WIDTH);
-  // }
-
   return (
-    <div className="photos">
-      <div className="album">
-      <div className="album__description">
-          <p>
-            31 августа АНО ПСД «От сердца к сердца» провели благотворительную акцию «Скоро в школу» для Землянской школы-интернат для обучающихся с ограниченными возможностями.<br/>
-            Сердечно благодарим каждого партнёра кто принимал участие!<br/>
-            Пусть отданное детям сегодня вернётся к вам завтра многократно приумноженным!
-          </p>
+    <div className="events">
+      <section className="event">
+        <div className="event__img">
+          <img src='/img/albums/zemlyansk/zemlyansk_01.jpg'/>
         </div>
-        <SRLWrapper>
-          <div className="album__preview">
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_01.jpg'/>
-            </div>
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_02.jpg'/>
-            </div>
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_03.jpg'/>
-            </div>
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_04.jpg'/>
-            </div>
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_05.jpg'/>
-            </div>
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_06.jpg'/>
-            </div>
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_07.jpg'/>
-            </div>
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_08.jpg'/>
-            </div>
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_09.jpg'/>
-            </div>
-            <div className="preview__img">
-              <img src='/img/albums/zemlyansk/zemlyansk_10.jpg'/>
-            </div>
-          </div>
-        </SRLWrapper>
-      </div>
+        <div className="event__description">
+          <h3 className="event__title">
+            КОУ ВО Землянская школа-интернат для обучающихся с ограниченными возможностями для здоровья
+          </h3>
+        </div>
+        <a href="#" className="event__show-more">
+          <span>подробнее</span>
+        </a>
+      </section>
     </div>
   )
 }
